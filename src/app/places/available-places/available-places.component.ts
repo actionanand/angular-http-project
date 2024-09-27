c5b5112cb9d434cd973b86cb0b05f04d9090a6d6
Anand raja <meetanandns@gmail.com>
2024-09-27 19:29:44 +0000

    backend api works

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,4 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { map, tap } from 'rxjs/operators';
 
 import { Place } from '../../models/place.model';
 import { PlacesComponent } from '../places.component';
@@ -11,6 +14,35 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+
+  private http = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+
+    const availablePlaceSub = this.http
+      .get<{ places: Place[] }>('/api/v2/places', {
+        observe: 'response',
+      })
+      .pipe(
+        tap(rawResp => {
+          console.log('Raw Response: ', rawResp);
+        }),
+        map(data => data.body),
+      )
+      .subscribe({
+        next: resp => {
+          this.places.set(resp?.places);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => availablePlaceSub.unsubscribe());
+  }
 }
--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -12,6 +12,8 @@ import { Place } from '../models/place.model';
 export class PlacesComponent {
   places = input.required<Place[]>();
   selectPlace = output<Place>();
+  // protected url = 'http://localhost:3000/';
+  protected url = 'https://3000-actionanand-angularhttp-six5y8k89a8.ws-us116.gitpod.io/';
 
   onSelectPlace(place: Place) {
     this.selectPlace.emit(place);
