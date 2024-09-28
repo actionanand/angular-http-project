a1237c86f1f9c25602c86bbb9fef8ea79aeaacc3
Anand raja <meetanandns@gmail.com>
2024-09-28 18:51:52 +0000

    interceptor added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
 
+function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
+  /*
+  const newReq = req.clone({
+    headers: req.headers.set('new-Header', 'headerValue')
+  });
+  */
+
+  console.log('Interceptor: ', req);
+  return next(req);
+}
+
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
 }).catch(err => console.error(err));
